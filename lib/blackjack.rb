a5c5c3ae4ff131b4cf794a38e71f2be1a11e762f
Erik Huang <erikhuangdrums@gmail.com>
2020-12-23 00:34:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,11 @@ def end_game(total)
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  total = card1 + card2
+  display_card_total(total)
+  total
 end
 
 def hit?
